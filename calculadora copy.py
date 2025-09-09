f8501e953afeb5656b28d25b4797187153d87082
henrick404 <160244828+henrick404@users.noreply.github.com>
2025-09-09 11:51:32 -0300

    Add files via upload

--- calculadora copy.py
+++ calculadora copy.py
@@ -6,69 +6,75 @@ def menu():
     action = inquirer.select(
         message = "operação:",
         choices = choices,
-        default=choices[0]
+        default = choices[0]
     ).execute()
 
     return action
 
 def calculo():
-    lista=[]
+    lista = []
     print(9*"*", "CALCULADORA", 9*"*")
-    repetir=True
-    vezesrepetidas=0
+    repetir = True
+    vezesrepetidas = 0
     while repetir:
         #parte visível
-        num1=int(input("número:"))        
+        num1 = float(input("número:"))        
         lista.append(num1)
-        operacao=menu()
+        operacao = menu()
         lista.append(operacao)
 
         #repetições para segundo numero
-        if vezesrepetidas==0:
-            num2=int(input("número:"))
+        if vezesrepetidas == 0:
+            num2 = float(input("número:"))
             lista.append(num2)
-            operacao=menu()
+            operacao = menu()
             lista.append(operacao)
         vezesrepetidas+=1
 
         #encerra o while
         if operacao== "=":
             repetir=False
-    resultado=0
+
     def operando(tipo):
         indice = 1
-        resultado=0
+        resultado=lista[0]
         while lista[indice] != "=":
+            print(lista)
             numero1=lista[indice-1]
             numero2=lista[indice+1]
             if tipo =="*":   
                 if lista[indice] == tipo:
                     resultado = numero1*numero2
-                    lista[indice-1]=resultado
                     del lista[indice+1]
                     del lista[indice]
+                    lista[indice-1]=resultado
+                
             if tipo =="/":   
                 if lista[indice] == tipo:
                     resultado = numero1/numero2
-                    lista[indice-1]=resultado
                     del lista[indice+1]
                     del lista[indice]
+                    lista[indice-1]=resultado
+
             if tipo =="+":   
                 if lista[indice] == tipo:
                     resultado = numero1+numero2
-                    lista[indice-1]=resultado
                     del lista[indice+1]
                     del lista[indice]
+                    lista[indice-1]=resultado
+
             if tipo =="-":   
                 if lista[indice] == tipo:
                     resultado = numero1-numero2  
-                    lista[indice-1]=resultado
                     del lista[indice+1]
-                    del lista[indice]                            
+                    del lista[indice]
+                    lista[indice-1]=resultado
+                           
     operando("*")
     operando("/")
     operando("+")
     operando("-")
+    print("Resultado é igual a:",lista[0])
     print(lista)
 
 calculo()
